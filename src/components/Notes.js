827c8dbd641567509462d235685544dbd7cdf331
Leo <ujfox13@gmail.com>
2022-01-11 23:21:19 +0300

    note

---
+++ src/components/Notes.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Notes = ({ notes }) => {
+
+    const notesItems = (notes) => {
+        return notes.map( (note) => { 
+            return (
+                <div key={note.id}>
+                    <li className="list-group-item d-flex justify-content-between align-items-center">
+                        <div className="d-flex justify-content-between w100 p-2">
+                            <strong>
+                                {note.title}
+                            </strong>
+                            <span>
+                                {new Date().toLocaleDateString()}
+                            </span>
+                        </div>
+                        <button 
+                            type="button" 
+                            className="btn btn-outline-danger">
+                                X
+                        </button>
+                    </li>
+                </div>
+            )
+        })
+    }
+
+    return (
+        <ul className="list-group">
+            {notesItems(notes)}
+        </ul>
+    );
+};
+
+export default Notes;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Form from '../components/Form';
+import Notes from '../components/Notes';
 
 const Home = () => {
+    const notes = new Array(4)
+        .fill('')
+        .map((item, i) => ({id: i, title: `Note ${i + 1}`}));
+
     return (
         <div>
-            <h1>
-                <Form/>
-            </h1>
+            <Form/>
+            <hr/>
+            <Notes notes={notes} />
         </div>
     );
 };
