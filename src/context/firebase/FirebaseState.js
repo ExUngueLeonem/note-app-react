0dfd7bd07e7c705f497f02a231327344301707b7
Leo <ujfox13@gmail.com>
2022-01-12 23:53:15 +0300

    fetchNotes bug in FirebaseState

--- src/context/firebase/FirebaseState.js
+++ src/context/firebase/FirebaseState.js
@@ -1,8 +1,10 @@
 import React, { useReducer } from "react";
+import axios from 'axios';
 import FirebaseContext from "./farebaseContext";
 import { firebaseReducer } from "./firebaseReducer";
+import { FETCH_NOTES, REMOVE_NOTE, SHOW_LOADER } from "../types";
 
-
+const url = process.env.REACT_APP_DB_URL
 
 const FirebaseState = ({ children }) => {
     const initialState = {
@@ -12,10 +14,39 @@ const FirebaseState = ({ children }) => {
 
     const [state, dispatch] = useReducer(firebaseReducer, initialState);
 
+    const showLoader = () => dispatch({type: SHOW_LOADER});
+
+    const fetchNotes = async () => {
+        showLoader();
+        const res = await axios.get( `${url}/notes.json`)
+
+        console.log(`fetchNotes`, res.data);
+    }
+
+    const addNote = async title => {
+        const note = {
+            title, date: new Date().toJSON()
+        }
+
+        const res = await axios.post(`${url}/notes.json`, note)
+        console.log( 'addNote', res.data);
+    }
+
+    const removeNote = async id => {
+        await axios.delete(`${url}/notes/${id}.json`)
+
+        dispatch({
+            type: REMOVE_NOTE,
+            payload: {id}
+        })
+    }
+
     return (
-        <FirebaseContext.Provider value={
-            ''
-        }>
+        <FirebaseContext.Provider value={{
+            showLoader, addNote, removeNote, fetchNotes,
+            loading: state.loading,
+            notes: state.notes
+        }}>
             {children}
         </FirebaseContext.Provider>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import FirebaseContext from '../context/firebase/farebaseContext';
+
 import Form from '../components/Form';
 import Notes from '../components/Notes';
+import Loader from '../components/Loader';
+
 
 const Home = () => {
-    const notes = new Array(4)
-        .fill('')
-        .map((item, i) => ({id: i, title: `Note ${i + 1}`}));
+    const {loading, notes, fetchNotes} = useContext(FirebaseContext);
+    
+    useEffect(() => {fetchNotes()},
+    //eslint-disable-next-line 
+    [])
 
+    console.log("loading", loading);
+    
     return (
-        <div>
+        <>
             <Form/>
             <hr/>
-            <Notes notes={notes} />
-        </div>
+            
+{/*             {loading 
+                ? <Loader /> 
+                : <Notes notes={notes} />
+            }
+ */}            
+                <Loader /> 
+
+        </>
     );
 };
 
