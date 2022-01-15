e1c9297acea50999cd646cf3fe9b3ce6a7f096dd
Leo <ujfox13@gmail.com>
2022-01-15 19:44:32 +0300

    cssTranslator animation

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,17 +1,28 @@
 import React, { useContext } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import { AlertContext } from '../context/alert/alertContext.js';
 
 const Alert = () => {
 const { alert, hide } = useContext(AlertContext);
 
-    if(!alert.visible) return null
     
     return (
+        <CSSTransition
+            in={alert.visible}
+            timeout={{
+                enter: 500,
+                exit: 350
+            }}
+            classNames={'alert'}
+            mountOnEnter
+            unmountOnExit
+        >
             <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`}>
                 <strong>Ахтунг!</strong> Вы что-то нажали, перезагрузите компьютер.&nbsp; 
                 {alert.text}
                 <button onClick={hide} type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
+        </CSSTransition>
     );
 };
 
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,33 +1,41 @@
 import React from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 const Notes = ({ notes, onRemove }) => {
     return (
-        
-        <ul className="list-group">
-        {notes.map( (note) => { 
-            return (
-                <div key={note.id}>
-                    <li className="list-group-item d-flex justify-content-between align-items-center">
-                        <div className="d-flex justify-content-between w100 p-2">
-                            <strong>
-                                {note.title}
-                            </strong>
-                            <small>
-                                {note.date}
-                            </small>
-                        </div>
-                        <button 
-                            type="button" 
-                            className="btn btn-outline-danger"
-                            onClick={() => onRemove(note.id)}
-                            >
-                                X
-                        </button>
-                    </li>
-                </div>
-            )
-        }).reverse()}
-        </ul>
+/*         <CSSTransition
+            timeout={750}
+            classNames={'alert'}
+            mountOnEnter
+            unmountOnExit
+        >
+ */            <ul className="list-group">
+            {notes.map( (note) => { 
+                return (
+                    <div key={note.id}>
+                        <li className="list-group-item d-flex justify-content-between align-items-center">
+                            <div className="d-flex justify-content-between w100 p-2">
+                                <strong>
+                                    {note.title}
+                                </strong>
+                                <small>
+                                    {note.date}
+                                </small>
+                            </div>
+                            <button 
+                                type="button" 
+                                className="btn btn-outline-danger"
+                                onClick={() => onRemove(note.id)}
+                                >
+                                    X
+                            </button>
+                        </li>
+                    </div>
+                )
+            }).reverse()}
+            </ul>
+/*         </CSSTransition>
+ */
     );
 };
 
